74c4b4cbb537dedb93da082689807c8f764204be
Cedric Arnould <sidekickinteractive@carnould.com>
2017-11-18 17:30:25 -0500

    added Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule, MatCardModule, MatInputModule, MatSnackBarModule, MatToolbarModule } from '@angular/material';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    MatButtonModule, MatCardModule, MatInputModule, MatSnackBarModule, MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
